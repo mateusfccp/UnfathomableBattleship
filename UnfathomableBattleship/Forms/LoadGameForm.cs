e1eb9c7cfe30dbee0d74c4575e0eaf74323accf5
David <leinerdavidgp@gmail.com>
2026-06-23 01:48:48 -0300

    LoadGame

--- UnfathomableBattleship/Forms/LoadGameForm.cs
+++ UnfathomableBattleship/Forms/LoadGameForm.cs
@@ -17,15 +17,36 @@ namespace UnfathomableBattleship.Forms
         {
             var partidas = _gameManager.GetCurrentPlayerGames();
 
-            flowLayoutPanel.Controls.Clear();
-
-            foreach (var partida in partidas)
+            if((partidas.Count == 0))
             {
-                // Creamos el panel contenedor de la tarjeta
+                flowLayoutPanel.Controls.Clear();
                 Panel card = new Panel
                 {
                     Width = flowLayoutPanel.Width - 25,
-                    Height = 80,
+                    Height = 90,
+                    BackColor = Color.Gray,
+                    Margin = new Padding(10)
+                };
+                Label infoLabel = new Label
+                {
+                    Text ="No hay partidas guardadas",
+                    ForeColor = Color.White,
+                    AutoSize = true,
+                    Location = new Point(10, 15),
+                    Font = new Font("Segoe UI", 10, FontStyle.Bold)
+                };
+                card.Controls.Add(infoLabel);
+                flowLayoutPanel.Controls.Add(card);
+                return;
+            }
+            
+
+            foreach (var partida in partidas)
+            {
+                Panel card = new Panel
+                {
+                    Width = flowLayoutPanel.Width - 25,
+                    Height = 90,
                     BackColor = Color.FromArgb(50, 70, 100),
                     Margin = new Padding(10)
                 };
@@ -37,8 +58,6 @@ namespace UnfathomableBattleship.Forms
                     Location = new Point(10, 15),
                     Font = new Font("Segoe UI", 10, FontStyle.Bold)
                 };
-
-                // Creamos el botón para cargar
                 Button btnCargar = new Button
                 {
                     Text = "Cargar",
@@ -50,12 +69,23 @@ namespace UnfathomableBattleship.Forms
                     Tag = partida.Id
                 };
 
-                // Le asignamos el evento click al botón
-                btnCargar.Click += BtnCargar_Click;
+                Button btnBorrar = new Button
+                {
+                    Text = "Borrar",
+                    BackColor = Color.Red,
+                    FlatStyle = FlatStyle.Flat,
+                    Size = new Size(100, 40),
+                    Location = new Point(btnCargar.Left - 120, 20),
+                    Cursor = Cursors.Hand,
+                    Tag = partida.Id
+                };
 
-                // Metemos los textos y el botón a la tarjeta, y la tarjeta al FlowLayoutPanel
+                btnCargar.Click += BtnCargar_Click;
+                btnBorrar.Click += BtnBorrar_Click;
+
                 card.Controls.Add(infoLabel);
                 card.Controls.Add(btnCargar);
+                card.Controls.Add(btnBorrar);
                 flowLayoutPanel.Controls.Add(card);
             }
         }
@@ -68,6 +98,16 @@ namespace UnfathomableBattleship.Forms
                 MainForm?.SwitchForm(new GameForm(_gameManager, _gameManager.LoadGame((sender as Button)?.Tag)));
                 Hide();
                 Dispose();
+            }
+        }
+
+        private void BtnBorrar_Click(object? sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Desea borrar esta partida?", "Confirmar", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (result == DialogResult.Yes)
+            {
+                _gameManager.DeleteGame((sender as Button)?.Tag);
+                LoadGames();
             }
         }
     }
